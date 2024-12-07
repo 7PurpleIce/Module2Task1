fad667a730c918749801384b4eeaa220646c7426
7PurpleIce <blackiceuwu@gmail.com>
2024-12-07 19:49:25 +0300

    rewrote task 9

--- vite-task1/src/App.jsx
+++ vite-task1/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { createElement } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,51 +9,52 @@ function App() {
   const [count, setCount] = useState(0)
   // Декларативный стиль: определяем состояние и используем хук useState
   const currentYear = new Date().getFullYear();
-  return React.createElement(
-    React.Fragment, 
+
+  return createElement(
+    'div',  // Корневой элемент
     null,
-    React.createElement(
+    createElement(
       'div',
       null,
-      React.createElement(
+      createElement(
         'a',
         { href: 'https://vite.dev', target: '_blank' },
-        React.createElement('img', { src: viteLogo, className: 'logo', alt: 'Vite logo' })
+        createElement('img', { src: viteLogo, className: 'logo', alt: 'Vite logo' })
       ),
-      React.createElement(
+      createElement(
         'a',
         { href: 'https://react.dev', target: '_blank' },
-        React.createElement('img', { src: reactLogo, className: 'logo react', alt: 'React logo' })
+        createElement('img', { src: reactLogo, className: 'logo react', alt: 'React logo' })
       )
     ),
-    React.createElement('h1', null, 'Vite + React'),
-    React.createElement(
+    createElement('h1', null, 'Vite + React'),
+    createElement(
       'div',
       { className: 'card' },
-      React.createElement(
+      createElement(
         'button',
         { onClick: () => setCount(count => count + 1) },
         `count is ${count}`
       ),
-      React.createElement(
+      createElement(
         'p',
         null,
         'Edit ',
-        React.createElement('code', null, 'src/App.jsx'),
+        createElement('code', null, 'src/App.jsx'),
         ' and save to test HMR'
       )
     ),
-    React.createElement(
+    createElement(
       'p',
       { className: 'read-the-docs' },
       'Click on the Vite and React logos to learn more'
     ),
-    React.createElement(
+    createElement(
       'footer',
       null,
-      React.createElement('p', null, currentYear)
+      createElement('p', null, currentYear)
     )
   )
 }
 
-export default App
+export default App;
